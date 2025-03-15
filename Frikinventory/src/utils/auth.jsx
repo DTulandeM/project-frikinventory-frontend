3988a83b8a972fc7402d7b34f16e96d810dd0336
DTulandeM <dtulande@gmail.com>
2025-03-15 01:02:38 -0500

    Trabajar con el servidor

--- Frikinventory/src/utils/auth.jsx
+++ Frikinventory/src/utils/auth.jsx
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:3000";
-// const BASE_URL = "https://api.friki.mooo.com";
+// const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://api.friki.mooo.com";
 
 export async function Signup(email, password) {
   try {
